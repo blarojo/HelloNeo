f0e9241e73afe3c2695219f1d99f3510b13b775f
piotr-muzyka <mrpmuzyka@gmail.com>
2021-10-21 23:39:50 +0200

    added welcome intent with audio code.

--- lambda/index.js
+++ lambda/index.js
@@ -3,6 +3,18 @@ const util = require('./util'); // utility functions
 const interceptors = require('./interceptors');
 const handlers = require('./handlers'); 
 
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+
+        const audioUrl = Util.getS3PreSignedUrl("Media/ubs-intro.mp3").replace(/&/g,'&amp;');
+        return handlerInput.responseBuilder
+                .speak(`<audio src="${audioUrl}"/>`)
+                .getResponse();
+    }
+};
 
 /**
  * This handler acts as the entry point for your skill, routing all request and response
@@ -36,3 +48,19 @@ exports.handler = Alexa.SkillBuilders.custom()
     .withApiClient(new Alexa.DefaultApiClient())
     .withCustomUserAgent('sample/happy-birthday/mod7')
     .lambda();
+
+
+const AskWeatherIntentHandler = {
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AskWeatherIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'The weather today is sunny.';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('The weather today is sunny.', speechText)
+      .getResponse();
+  }
+};
