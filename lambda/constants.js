6a025676776370d4f694f319a60f31afbef28445
Aditya <aditya05khajuria@gmail.com>
2021-10-22 13:29:59 +0530

    S3

--- lambda/constants.js
+++ lambda/constants.js
@@ -1,10 +1,8 @@
 module.exports = {
     // we now specify which attributes are saved (see the save interceptor below)
-    PERSISTENT_ATTRIBUTES_NAMES: ['day', 'month', 'monthName', 'year', 'sessionCounter', 'reminderId'],
+    PERSISTENT_ATTRIBUTES_NAMES: ['amazon', 'tesla', 'ibm', 'ubs', 'youtube', 'whatsapp', 'sessionCounter', 'reminderId'],
     // these are the permissions needed to fetch the first name
     GIVEN_NAME_PERMISSION: ['alexa::profile:given_name:read'],
     // these are the permissions needed to send reminders
-    REMINDERS_PERMISSION: ['alexa::alerts:reminders:skill:readwrite'],
-    // max number of entries to fetch from the external API
-    MAX_BIRTHDAYS: 5
+    REMINDERS_PERMISSION: ['alexa::alerts:reminders:skill:readwrite']
 }
